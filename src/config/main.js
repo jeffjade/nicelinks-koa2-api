30f1a62190f220f9ffde25cd8bc64fce60829ace
jade.yang <yunjeff@163.com>
2017-08-16 12:04:36 +0800

    feat(static): update static 😄

--- src/config/main.js
+++ src/config/main.js
@@ -1,27 +1,29 @@
 let { join } = require('path')
 
 let config = {
-env: 'development',
+  env: 'development',
 
-// Secret key for JWT signing and encryption
-'secret': 'super secret passphrase',
+  // Secret key for JWT signing and encryption
+  'secret': 'super secret passphrase',
 
-// Database connection information
-'database': 'mongodb://localhost:27017/nice_links',
+  // Database connection information
+  'database': 'mongodb://localhost:27017/nice_links',
 
-// Setting port for server
-'port': process.env.PORT || 4000,
+  // Setting port for server
+  'port': process.env.PORT || 4000,
 
-// Avatar upload path
-'avatarUploadDir': join(__dirname, 'upload/avatar'),
+  // Avatar upload path
+  'avatarUploadDir': join(__dirname, 'upload/avatar'),
 
-//  Email path
-'clientPath': 'http://localhost:8888'
+  //  Email path
+  'clientPath': 'http://localhost:8888'
 }
 
+console.log(process.env.NODE_ENV)
+
 if (process.env.NODE_ENV === 'production') {
-  config.env = production
-  config.clientPath = 'http://nicelinks.site'
+  config.env = 'production'
+  config.clientPath = 'https://nicelinks.site'
 }
 
 module.exports = config
